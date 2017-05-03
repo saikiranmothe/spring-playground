aca51d284da00599db303aaa4eb016f42492e33a
saikiranmothe <saikiran.mothe@gmail.com>
2017-05-03 06:20:45 -0600

    endpoints tests

--- src/main/java/com/example/EndpointsController.java
+++ src/main/java/com/example/EndpointsController.java
@@ -5,16 +5,23 @@
 
 package com.example.controller;
 
+
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
+import java.lang.String;
+
+
 @RestController
 public class EndpointsController {
 
+
     @GetMapping("/math/pi")
-    public String piValue()
+    public String PIvalue()
     {
         return  "3.141592653589793";
     }
--- src/test/java/com/example/EndpointsControllerTest.java
+++ src/test/java/com/example/EndpointsControllerTest.java
@@ -1,28 +1,43 @@
+package com.example;
+
+import com.example.controller.EndpointsController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import java.util.ArrayList;
+import java.util.List;
+
+
+import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
-@RunWith(SpringRunner.class)
 @WebMvcTest(EndpointsController.class)
+@RunWith(SpringRunner.class)
 public class EndpointsControllerTest {
 
     @Autowired
-    private MockMvc mvc;
+    private
+    MockMvc mvc;
 
     @Test
-    public void testpiValue() throws Exception {
-        this.mvc.perform(get("/math/pi").accept(MediaType.TEXT_PLAIN))
+    public void testPIvalue() throws Exception{
+
+        RequestBuilder request = MockMvcRequestBuilders.get("/math/pi");
+
+        this.mvc.perform(request)
                 .andExpect(status().isOk())
                 .andExpect(content().string("3.141592653589793"));
-    }
 
+
+    }
 }
